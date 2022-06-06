689c48dfbec0dac24392e5171edc6eff819e0048
vaisakhsvarma <vaisakhsvarma@gmail.com>
2022-06-06 17:25:28 +0530

    added create action for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,16 @@ class SessionsController < ApplicationController
     def home
     end
 
+    def create
+        @user = User.find_by(username: params[:username])
+
+        if !!@user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path
+       else
+           message = "Something went wrong! Please check your username and password and try again!"
+           redirect_to login_path, notice: message 
+       end
+    end
+
 end
