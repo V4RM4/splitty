eb17fb5c6cd7cbe89925b8b6104a4af3e730e653
vaisakhsvarma <vaisakhsvarma@gmail.com>
2022-06-08 00:45:11 +0530

    modified routes and added welcome to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'sessions#home'
-
-  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
-
-  get "/login", to: "sessions#login"
-  get "/logout", to: "sessions#destroy"
-
-  post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
+  resources :users, only: [:new, :create, :index, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
