3cf641622d9aba258bc413122e8ea401385c449a
vaisakhsvarma <vaisakhsvarma@gmail.com>
2022-06-08 00:57:55 +0530

    improved user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,29 @@
 class UsersController < ApplicationController
 
+    def show
+    end
+  
     def index
+    end
+  
+    def new
         @user = User.new
     end
-
+  
     def create
-        @user = User.new(user_params)
-        if @user.save
+        @user = User.create(user_params)
+        if @user.valid?
             session[:user_id] = @user.id
-            redirect_to root_path
+            redirect_to @user
         else
-            render :new
+            flash[:error] = "Error- please try to create an account again."
+            redirect_to new_user_path
         end
     end
-
-    def show
-        @user = User.find(params[:id])
-    end
-
-    private
+  
+    private 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:email, :password)
     end
+
 end
